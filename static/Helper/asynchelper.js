1313159316482097e627e98df0db34dab2bc96a7
MS07062000 <singhmurali.14@gmail.com>
2023-06-30 22:34:37 +0530

    activated search on home page, fixed header issues

--- static/Helper/asynchelper.js
+++ static/Helper/asynchelper.js
@@ -16,22 +16,31 @@ export async function headerInitialization() {
         loggedState = true;
         document.body.querySelector(".userName").textContent = userName;
     }
-
+    if (window.innerWidth >= 1024) {
+        logged(loggedState);
+    }
     // console.log(loggedState);
 
     document.body.querySelector(".logout").addEventListener("click", (event) => {
         fetch("/api/logout").then((result) => {
-            console.log(result);
-            changeWindowLocation();
+            if(result.status==200){
+                changeWindowLocation();
+            }
         });
     });
 
-    if (window.innerWidth >= 1024) {
-        logged(loggedState);
-    }else{
-        document.body.querySelector(".loggedIn").style.display = "none";
-        document.body.querySelector(".loggedOut").style.display = "none";
-    }
+    window.addEventListener('resize',(event)=>{
+        if (window.innerWidth >= 1024) {
+            document.body.querySelector(".hamburgermenu").style.display = "none";
+            logged(loggedState);
+        }else{
+            document.body.querySelector(".loggedIn").style.display = "none";
+            document.body.querySelector(".loggedOut").style.display = "none";
+            document.body.querySelector(".hamburgermenu").style.display = "flex";
+        }
+    });
+
+    
     
     let hamburgermenu = document.body.querySelector('.hamburger');
     let crossSign = document.body.querySelector('.crossSign');
@@ -39,7 +48,6 @@ export async function headerInitialization() {
         hamburgermenu.style.display = "none";
         crossSign.style.display = "block";
         logged(loggedState);
-      
     });
 
     crossSign.addEventListener('click', (event) => {
@@ -63,15 +71,12 @@ function logged(loggedState){
 
 function changeWindowLocation() {
     let currentLocation = window.location.pathname;
-    console.log(currentLocation);
+    // console.log(currentLocation);
     let pagelocation = ["/addCampground", "/addComment"];
     // addComment doubt 
     if (pagelocation.indexOf(currentLocation) == -1) {
-        // location.reload(); // looping is happening don't know how
+        location.href = "/search";
     } else {
         location.href = "/signIn";
     }
 }
-
-
-
--- static/SearchPage/search.js
+++ static/SearchPage/search.js
@@ -1,11 +1,99 @@
-var response=await fetch("/api/allCampgrounds",{method:"GET"});
-var data= await response.json();
-// console.log(data);
+async function search() {
+    var response = await fetch("/api/allCampgrounds", { method: "GET" });
+    var data = await response.json();
+    // console.log(data);
+    Array.from(document.querySelector(".campGrounds").children).forEach((childElement) => { childElement.remove(); });
+    if (data.length != 0) {
+        data.forEach((campground) => {
+            let campcard = document.createElement("camp-card");
+            campcard.setAttribute("campName", campground.campgroundName);
+            campcard.setAttribute("campDescription", campground.description);
+            campcard.setAttribute("img", campground.image);
+            document.querySelector(".campGrounds").appendChild(campcard);
+        });
+    } else {
+        let note = document.createElement("p");
+        note.textContent = "No campground available.";
+        note.setAttribute("name", "note");
+        document.querySelector(".campGrounds").appendChild(note);
 
-data.forEach((campground) => {
-    let campcard=document.createElement("camp-card");
-    campcard.setAttribute("campName",campground.campgroundName);
-    campcard.setAttribute("campDescription",campground.description);
-    campcard.setAttribute("img",campground.image);
-    document.querySelector(".campGrounds").appendChild(campcard);
-});
+    }
+
+
+    var timerId;
+    var searchText = document.body.querySelector('input[class=search]');
+    searchText.addEventListener("input", (event) => {
+        console.log(searchText.value);
+        clearTimeout(timerId);
+        // timerId  =  setTimeout(async ()=>{
+        //     if(searchText.value.trim().length!=0){
+        //         response=await fetch(`/api/search/${searchText.value}`,{method:"GET"});
+        //     }else{
+        //         response=await fetch("/api/allCampgrounds",{method:"GET"});
+        //     }
+        //     data= await response.json();
+        //     Array.from(document.querySelector(".campGrounds").children).forEach((childElement)=>{childElement.remove();});
+        //     if(data.length!=0){
+        //         data.forEach((campground) => {
+        //             let campcard=document.createElement("camp-card");
+        //             campcard.setAttribute("campName",campground.campgroundName);
+        //             campcard.setAttribute("campDescription",campground.description);
+        //             campcard.setAttribute("img",campground.image);
+        //             document.querySelector(".campGrounds").appendChild(campcard);
+        //         });
+        //     }else{
+        //         let note=document.createElement("p");
+        //         note.textContent="No campground available as per your input";
+        //         note.setAttribute("name","note");
+        //         document.querySelector(".campGrounds").appendChild(note);
+        //     }
+        // }, 200);
+
+        timerId = setTimeout(async () => {
+            if (searchText.value.trim().length != 0) {
+                response = await fetch(`/api/search/${searchText.value}`, { method: "GET" });
+                data= await response.json();
+            }else{
+                data=[];
+            }
+
+            displaySearchResults(data);
+        });
+    });
+}
+search();
+
+
+
+
+function displaySearchResults(results) {
+    let resultsContainer = document.body.querySelector(".search-results");
+    Array.from(resultsContainer.children).forEach((childElement)=>{childElement.remove();});
+    if (results.length === 0) {
+        if(!resultsContainer.classList.contains('hide-search-results')){
+            resultsContainer.classList.add('hide-search-results');
+        }
+        return;
+    }
+
+    let ul = document.createElement("ul");
+    ul.classList.add("search-results-list");
+
+
+    results.forEach(function (result) {
+        let li = document.createElement("li");
+        li.textContent = result.campgroundName;
+        li.classList.add("search-results-item");
+        li.addEventListener("click",(event)=>{
+            location.href=`/campground/${encodeURIComponent(result.campgroundName)}`;
+        });
+        ul.appendChild(li);
+    });
+
+    resultsContainer.appendChild(ul);
+
+    if(resultsContainer.classList.contains('hide-search-results')){
+        resultsContainer.classList.remove('hide-search-results');
+    }
+
+}
