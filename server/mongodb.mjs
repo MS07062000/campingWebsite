2fe91b5857757a81873aae5def1c22a95c110344
MS07062000 <singhmurali.14@gmail.com>
2023-07-02 23:06:29 +0530

    fixed path for env files in mongodb.mjs

--- server/mongodb.mjs
+++ server/mongodb.mjs
@@ -1,6 +1,12 @@
 import { MongoClient } from 'mongodb';
 import dotenv from 'dotenv';
-dotenv.config();
+import path, { resolve } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+dotenv.config({ path: resolve(__dirname, '.env') });
+// dotenv.config();
 // Create a new MongoClient
 const client = new MongoClient(process.env.MONGODB_URI);
 export let db;
