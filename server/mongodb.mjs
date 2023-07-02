1a24b1c1f960d30756f7f3d502e5d562eb2cdab2
MS07062000 <singhmurali.14@gmail.com>
2023-07-02 20:50:55 +0530

    CHANGED URI IN MONGODB

--- server/mongodb.mjs
+++ server/mongodb.mjs
@@ -1,16 +1,12 @@
 import { MongoClient } from 'mongodb';
 import dotenv from 'dotenv';
 dotenv.config();
-// Connection URI
-const uri =
-  `mongodb://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_HOST}:${process.env.MONGODB_PORT}/${process.env.MONGODB_DATABASE}`;
 // Create a new MongoClient
-const client = new MongoClient(uri);
+const client = new MongoClient(process.env.MONGODB_URI);
 export let db;
 
 export async function connectToDatabase () {
   try {
-    console.log(uri);
     await client.connect();
     console.log('Connected');
     await client.db('admin').command({ ping: 1 });
